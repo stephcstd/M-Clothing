c1a1509a079906f436137feaf9c3c7ca8a92df9e
stephcstd <markstephencustodio17@gmail.com>
2021-01-15 08:54:09 +0800

    session 23 completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import SigninScreen from './screens/SigninScreen';
 import { signout } from './actions/userActions';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingAddressScreen from './screens/ShippingAddressScreen';
+import PaymentMethodScreen from './screens/PaymentMethodScreen';
 
 function App() {
 
@@ -56,6 +57,7 @@ function App() {
             <Route path="/signin" component={SigninScreen}></Route>
             <Route path="/register" component={RegisterScreen}></Route>
             <Route path="/shipping" component={ShippingAddressScreen}></Route>
+            <Route path="/payment" component={PaymentMethodScreen}></Route>
             <Route path="/" component={HomeScreen} exact></Route>
           </main>
           <footer className="row center">All rights reserved.</footer>
--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -47,8 +47,10 @@ export const signin = (email, password) => async (dispatch) => {
     });
   }
 };
+
 export const signout = () => (dispatch) => {
   localStorage.removeItem('userInfo');
   localStorage.removeItem('cartItems');
+  localStorage.removeItem('shippingAddress');
   dispatch({ type: USER_SIGNOUT });
 };
