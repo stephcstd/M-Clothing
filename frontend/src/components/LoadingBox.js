1660992edaf4c5ba890811e32decf93b59f2d973
stephcstd <markstephencustodio17@gmail.com>
2021-01-15 08:06:41 +0800

    session 21 completed

--- frontend/src/components/LoadingBox.js
+++ frontend/src/components/LoadingBox.js
@@ -1,6 +1,6 @@
 export default function LoadingBox(){
     return(
-        <div>
+        <div className="loading">
             <i className="fa fa-spinner fa-spin"></i> Loading...
         </div>
     );
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,7 @@ import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
 import thunk from 'redux-thunk';
 import {productDetailsReducer, productListReducer} from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
-import { userSigninReducer } from './reducers/userReducers';
+import { userRegisterReducer, userSigninReducer } from './reducers/userReducers';
 
 const initialState = {
     userSignin: {
@@ -22,6 +22,7 @@ const reducer = combineReducers({
     productDetails:productDetailsReducer,
     cart: cartReducer,
     userSignin: userSigninReducer,
+    userRegister: userRegisterReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
