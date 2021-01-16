b1f424cc74b6a1a7de99dd3c5e1630860de97a9b
stephcstd <markstephencustodio17@gmail.com>
2021-01-17 06:06:40 +0800

    session 38 completed

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import userRouter from './routers/userRouter.js';
 import productRouter from './routers/productRouter.js';
 import dotenv from 'dotenv';
 import orderRouter from './routers/orderRouter.js';
+import path from 'path';
+import uploadRouter from './routers/uploadRouter.js';
 
 dotenv.config();
 
@@ -18,6 +20,7 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/m-clothing', {
   useCreateIndex: true,
 });
 
+app.use('/api/uploads', uploadRouter);
 
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
@@ -26,6 +29,9 @@ app.use('/api/orders', orderRouter);
 app.get('/api/config/paypal', (req, res) => {
     res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
+
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.get('/', (req, res)=> {
     res.send('Server is ready');
